a6b88cad4592efc1acb7311aad628af3498eb90a
betterjohn <66306036+betterjohn@users.noreply.github.com>
2021-11-09 21:51:45 +0800

    login initial

--- controller/UserController.go
+++ controller/UserController.go
@@ -8,6 +8,7 @@ import (
 	"com.mego.first/megofirst/model"
 	"com.mego.first/megofirst/util"
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
 
@@ -42,10 +43,17 @@ func Register(ctx *gin.Context) {
 		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "user already exist"})
 		return
 	}
+
+	hashedPawword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"code": 500, "msg": "error happening on password encryption"})
+		return
+	}
+
 	newUser := model.User{
 		Username: username,
 		Mobile:   mobile,
-		Password: password,
+		Password: string(hashedPawword),
 	}
 
 	db.Create(&newUser)
@@ -53,6 +61,54 @@ func Register(ctx *gin.Context) {
 	ctx.JSON(200, gin.H{
 		"message": "success",
 	})
+}
+
+func Login(ctx *gin.Context) {
+	db := common.InitDB()
+	// get params
+	username := ctx.PostForm("username")
+	password := ctx.PostForm("password")
+
+	// params ok?
+	if len(password) < 8 {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "password too weak"})
+		return
+	}
+
+	if len(username) == 0 {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "username cannot be empty"})
+		return
+	}
+
+	// if exist?
+	var user model.User
+	db.Where("username = ?", username).First(&user)
+
+	if user.ID == 0 {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "user not exist"})
+		return
+	}
+
+	// password correct?
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"code": 400, "msg": "username / password not match"})
+		return
+	}
+
+	// generate token
+	token := "token"
+
+	// response
+	ctx.JSON(
+		http.StatusOK,
+		gin.H{
+			"code": 200,
+			"msg":  "login success",
+			"data": gin.H{
+				"token": token,
+			},
+		},
+	)
 }
 
 func isMobileExist(db *gorm.DB, mobile string) bool {
